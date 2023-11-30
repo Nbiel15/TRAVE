d8d3de172ad9d0f70d67b65411f17f2b40462e3e
Nbiel15 <126222340+Nbiel15@users.noreply.github.com>
2023-11-30 07:45:18 +0700

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,25 @@
+<script>
+
+
+
+Let greyBtn = document.getElementById("grey");
+
+Let redBtn = document.getElementById("red");
+
+Let greenBtn = document.getElementById("green");
+
+Let bike document.getElementById("bike");
+
+greyBtn.onclick = function () {
+
+bike.style.backgroundImage = "url(image/bg1.png)"; };
+
+redBtn.onclick = function () {
+
+bike.style.backgroundImage = "url(image/bg2.png)"; };
+
+greenBtn.onclick = function () {
+
+bike.style.backgroundImage = "url(image/bg3.png)"; };
+
+</script>
